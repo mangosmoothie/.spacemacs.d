c0d3c8697aa83a32af12238c3789d833718196cb
mangosmoothie <mangosmoothie@users.noreply.github.com>
2017-01-17 23:28:05 -0600

    removing typo

--- init.el
+++ init.el
@@ -16,7 +16,7 @@ values."
    ;; and `nil'. `unused' will lazy install only unused layers (i.e. layers
    ;; not listed in variable `dotspacemacs-configuration-layers'), `all' will
    ;; lazy install any layer that support lazy installation even the layers
-  resources ;; listed in `dotspacemacs-configuration-layers'. `nil' disable the lazy
+   ;; listed in `dotspacemacs-configuration-layers'. `nil' disable the lazy
    ;; installation feature and you have to explicitly list a layer in the
    ;; variable `dotspacemacs-configuration-layers' to install it.
    ;; (default 'unused)
